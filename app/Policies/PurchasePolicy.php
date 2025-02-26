36d86b7400f46290a8cb0dc0e561993e5540cd16
SulaimanQasimi <farzadqasimy@gmail.com>
2025-02-26 19:55:26 +0430

    `Update PurchasePolicy to allow all actions for all users`

--- app/Policies/PurchasePolicy.php
+++ app/Policies/PurchasePolicy.php
@@ -13,7 +13,7 @@ class PurchasePolicy
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,7 +21,7 @@ class PurchasePolicy
      */
     public function view(User $user, Purchase $purchase): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -29,7 +29,7 @@ class PurchasePolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -37,7 +37,7 @@ class PurchasePolicy
      */
     public function update(User $user, Purchase $purchase): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -45,7 +45,7 @@ class PurchasePolicy
      */
     public function delete(User $user, Purchase $purchase): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -53,7 +53,7 @@ class PurchasePolicy
      */
     public function restore(User $user, Purchase $purchase): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -61,6 +61,6 @@ class PurchasePolicy
      */
     public function forceDelete(User $user, Purchase $purchase): bool
     {
-        return false;
+        return true;
     }
 }
