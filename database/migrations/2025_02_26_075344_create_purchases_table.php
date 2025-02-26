f5bbbee4570709e0704ffe48ac23cab7cb215c03
SulaimanQasimi <farzadqasimy@gmail.com>
2025-02-26 18:53:11 +0430

    Add Purchase migration

---
+++ database/migrations/2025_02_26_075344_create_purchases_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Currency;
+use App\Models\Supplier;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->nullable();
+            $table->foreignIdFor(Supplier::class)->nullable();
+            $table->foreignIdFor(Currency::class)->nullable();
+            $table->string("invoice_number")->nullable();
+            $table->date("invoice_date")->nullable();
+
+            $table->string("per_currecy_rate")->nullable();
+            $table->string("total_amount")->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
