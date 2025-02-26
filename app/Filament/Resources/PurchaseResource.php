001d8da5754576892f41a5ecbd49b9e58e2dd70c
SulaimanQasimi <farzadqasimy@gmail.com>
2025-02-26 19:48:59 +0430

    Add resource of Purchase

---
+++ app/Filament/Resources/PurchaseResource.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PurchaseResource\Pages;
+use App\Filament\Resources\PurchaseResource\RelationManagers;
+use App\Models\Purchase;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PurchaseResource extends Resource
+{
+    protected static ?string $model = Purchase::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('user_id')
+                    ->numeric(),
+                Forms\Components\TextInput::make('supplier_id')
+                    ->numeric(),
+                Forms\Components\TextInput::make('currency_id')
+                    ->numeric(),
+                Forms\Components\TextInput::make('invoice_number')
+                    ->maxLength(255),
+                Forms\Components\DatePicker::make('invoice_date'),
+                Forms\Components\TextInput::make('currecy_rate')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('total_amount')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('status')
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('supplier_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('currency_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('invoice_number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('invoice_date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('currecy_rate')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('total_amount')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPurchases::route('/'),
+            'create' => Pages\CreatePurchase::route('/create'),
+            'view' => Pages\ViewPurchase::route('/{record}'),
+            'edit' => Pages\EditPurchase::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
