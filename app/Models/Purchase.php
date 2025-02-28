b73ec153f62d55994e77115c0575955c46edb560
SulaimanQasimi <farzadqasimy@gmail.com>
2025-02-28 16:35:40 +0430

    `Added relationships to Purchase model`

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Purchase extends Model
 {
     use SoftDeletes;
+
+    public function currency()
+    {
+        return $this->belongsTo(Currency::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
 }
