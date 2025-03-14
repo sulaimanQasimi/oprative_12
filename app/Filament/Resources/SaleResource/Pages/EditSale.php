5c7500666b137d6b9e0bb3f6a906481523ff356a
SulaimanQasimi <farzadqasimy@gmail.com>
2025-03-14 21:38:27 +0430

    `Added mount method to EditSale page to display notification and redirect when sale status is 'completed'`

--- app/Filament/Resources/SaleResource/Pages/EditSale.php
+++ app/Filament/Resources/SaleResource/Pages/EditSale.php
@@ -5,10 +5,25 @@ namespace App\Filament\Resources\SaleResource\Pages;
 use App\Filament\Resources\SaleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Notifications\Notification;
 
 class EditSale extends EditRecord
 {
     protected static string $resource = SaleResource::class;
+
+    public function mount($record): void
+    {
+        parent::mount($record);
+
+        if ($this->record->status === 'completed') {
+            Notification::make()
+                ->warning()
+                ->title(trans('Already Completed'))
+                ->body(trans('This sale has already been moved to stock.'))
+                ->send();
+            $this->redirect($this->getResource()::getUrl('view', ['record' => $record]));
+        }
+    }
 
     protected function getHeaderActions(): array
     {
