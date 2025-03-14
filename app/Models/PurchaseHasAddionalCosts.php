a062fb506fba8aa24e0386cef625072d2547b2a4
SulaimanQasimi <farzadqasimy@gmail.com>
2025-03-14 20:38:50 +0430

    "Removed a line of whitespace and corrected the indentation in PurchaseHasAddionalCosts model."

--- app/Models/PurchaseHasAddionalCosts.php
+++ app/Models/PurchaseHasAddionalCosts.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class PurchaseHasAddionalCosts extends Model
 {
     public $timestamps = false;
-
     protected $fillable = [
         'purchase_id',
         'name',
