140742be997d3c1d8c2d374ef0998912b0b5d86a
SulaimanQasimi <farzadqasimy@gmail.com>
2025-03-21 21:38:08 +0430

    fix(migration): correct method call for name column in products table migration
    Update the migration for the products table to replace the incorrect json method with the correct name method for the name column, ensuring proper schema definition.

--- database/migrations/2025_02_26_073337_create_products_table.php
+++ database/migrations/2025_02_26_073337_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
 
             $table->string("type")->default('product')->nullable();
-            $table->json("name");
+            $table->name("name");
             $table->string("barcode")->nullable();
 
             //Prices
