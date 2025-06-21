7a99c132628a9336dbee6fe8debd159340c07791
SulaimanQasimi <farzadqasimy@gmail.com>
2025-06-21 19:51:22 +0430

    refactor: streamline warehouse permissions and roles in seeder
    Removed unnecessary permissions and role assignments from the WarehousePermissionSeeder to simplify the permission structure. This change focuses on essential permissions for warehouse operations, enhancing clarity and maintainability of the seeder code.

--- database/seeders/WarehousePermissionSeeder.php
+++ database/seeders/WarehousePermissionSeeder.php
@@ -19,32 +19,20 @@ class WarehousePermissionSeeder extends Seeder
 
         // Create warehouse permissions
         $warehousePermissions = [
-            // Authentication permissions
-            'warehouse.login',
-            'warehouse.logout',
 
             // Dashboard permissions
             'warehouse.view_dashboard',
 
             // Product permissions
             'warehouse.view_products',
-            'warehouse.create_products',
-            'warehouse.edit_products',
-            'warehouse.delete_products',
-
             // Income permissions
             'warehouse.view_income',
-            'warehouse.view_income_details',
 
             // Outcome permissions
             'warehouse.view_outcome',
-            'warehouse.view_outcome_details',
 
             // Sales permissions
             'warehouse.view_sales',
-            'warehouse.create_sales',
-            'warehouse.edit_sales',
-            'warehouse.delete_sales',
             'warehouse.view_sale_details',
             'warehouse.generate_invoice',
             'warehouse.confirm_sales',
@@ -57,68 +45,6 @@ class WarehousePermissionSeeder extends Seeder
         // Create all permissions
         foreach ($warehousePermissions as $permission) {
             Permission::create(['name' => $permission, 'guard_name' => 'warehouse_user']);
-        }
-
-        // Create warehouse roles
-        $adminRole = Role::create(['name' => 'warehouse_admin', 'guard_name' => 'warehouse_user']);
-        $managerRole = Role::create(['name' => 'warehouse_manager', 'guard_name' => 'warehouse_user']);
-        $employeeRole = Role::create(['name' => 'warehouse_employee', 'guard_name' => 'warehouse_user']);
-        $salesRole = Role::create(['name' => 'warehouse_sales', 'guard_name' => 'warehouse_user']);
-
-        // Admin gets all permissions
-        $adminRole->syncPermissions($warehousePermissions);
-
-        // Manager permissions
-        $managerRole->syncPermissions([
-            'warehouse.login',
-            'warehouse.logout',
-            'warehouse.view_dashboard',
-            'warehouse.view_products',
-            'warehouse.create_products',
-            'warehouse.edit_products',
-            'warehouse.view_income',
-            'warehouse.view_income_details',
-            'warehouse.view_outcome',
-            'warehouse.view_outcome_details',
-            'warehouse.view_sales',
-            'warehouse.view_sale_details',
-            'warehouse.generate_invoice',
-            'warehouse.confirm_sales',
-            'warehouse.view_profile',
-            'warehouse.edit_profile',
-        ]);
-
-        // Employee permissions (limited)
-        $employeeRole->syncPermissions([
-            'warehouse.login',
-            'warehouse.logout',
-            'warehouse.view_dashboard',
-            'warehouse.view_products',
-            'warehouse.view_income',
-            'warehouse.view_outcome',
-            'warehouse.view_profile',
-            'warehouse.edit_profile',
-        ]);
-
-        // Sales role permissions
-        $salesRole->syncPermissions([
-            'warehouse.login',
-            'warehouse.logout',
-            'warehouse.view_dashboard',
-            'warehouse.view_products',
-            'warehouse.view_sales',
-            'warehouse.create_sales',
-            'warehouse.edit_sales',
-            'warehouse.view_sale_details',
-            'warehouse.generate_invoice',
-            'warehouse.view_profile',
-            'warehouse.edit_profile',
-        ]);
-
-        // Assign admin role to first warehouse user (for testing)
-        $user = WarehouseUser::find(1);
-        if ($user) {
-            $user->assignRole('warehouse_admin');
         }
     }
 } 
