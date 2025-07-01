0145eda6250a7233b4e7765da9319ab78007b6af
Mohammad  Rafi10 <87257987+sulaimanQasimi@users.noreply.github.com>
2025-07-01 11:20:48 +0430

    fix: update SalesIndex component to reflect confirmed status
    Modified the SalesIndex component to change the displayed status from 'Status' to 'Confirmed by Store'. This update enhances clarity for users regarding the confirmation status of sales, aligning with recent feature enhancements.

--- resources/js/Pages/Customer/Sales/Index.jsx
+++ resources/js/Pages/Customer/Sales/Index.jsx
@@ -1033,10 +1033,10 @@ export default function SalesIndex({
                                                                     strokeLinecap="round"
                                                                     strokeLinejoin="round"
                                                                     strokeWidth="2"
-                                                                    d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                                                                    d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
                                                                 />
                                                             </svg>
-                                                            {t('Status')}
+                                                            {t('Confirmed by Store')}
                                                         </div>
                                                     </th>
                                                     <th
